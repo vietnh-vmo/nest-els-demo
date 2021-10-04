7f7d774c3f774211791d3798b0342d9ab6d344e5
Viet Nguyen <vietnh@vmodev.com>
2021-10-04 12:59:27 +0700

    fix/update-es-path

--- src/modules/search/search.module.ts
+++ src/modules/search/search.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
+import { envConfig } from '@helper/env.helpers';
 
 @Module({
   imports: [
     ElasticsearchModule.registerAsync({
       useFactory: async () => ({
-        node: 'http://localhost:9200',
+        node: envConfig.ES_URL,
         maxRetries: 10,
         requestTimeout: 60000,
         pingTimeout: 60000,
